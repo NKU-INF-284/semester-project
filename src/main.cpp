0a8803273945cdc8a9cafd5cc62923e907299eff
Zack Sargent <zackarywsargent@gmail.com>
2022-10-31 20:38:52 -0400

    add imports for future use

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include "main.hpp"
 
 int main(void) {
+    // Steps: (I think)
+
+    // create file descriptor
+    // make call to `getaddrinfo`
+    // loop through all results and bind to first avaliable
+    // forever, accept connections
+
     std::cout << "Hello World!\n";
     std::cout << "from INF 284\n";
 }
---
+++ src/main.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Combined from https://beej.us/guide/bgnet/examples/
+// These are mostly unused, so far
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>

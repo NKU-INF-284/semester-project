90f2a928196c5624f99837a71132d8683fd00691
Zack Sargent <zackarywsargent@gmail.com>
2022-11-01 00:03:59 -0400

    bind socket to file descriptor and wait for connections

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,26 @@
 
 #include "server.hpp"
 
+/**
+ * Beej's Guide to Network Programming
+ * https://beej.us/guide/bgnet/examples/server.c
+ */
+void sigchld_handler(int s) {
+    (void)s;  // quiet unused variable warning
+
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
+
+    errno = saved_errno;
+}
+
 int main(void) {
     /**
      * Get address info:
-     * (Note this is still heavily taken from Beej
+     * (Note this is still mostly taken from Beej
      * (https://beej.us/guide/bgnet/examples/))
      */
     struct addrinfo hints;
@@ -21,8 +37,60 @@ int main(void) {
         return 1;
     }
 
-    // create file descriptor
-    // make call to `getaddrinfo`
+    /**
+     * Bind the socket to a file descriptor
+     * (Note this is still mostly taken from Beej
+     * (https://beej.us/guide/bgnet/examples/))
+     */
+    int sockfd;
+    struct addrinfo *p;
+    struct sigaction sa;
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) ==
+            -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        int yes = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) ==
+            -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo);  // all done with this structure
+
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler;  // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
     // loop through all results and bind to first avaliable
     // forever, accept connections
 
